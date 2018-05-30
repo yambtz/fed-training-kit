fb6009bc485897ba6b2e5bf3603459cd02013860
Yam Ben-Tzvi <yamb@Yam-b-mbp15.local>
2018-05-30 12:34:39 +0300

    remove quotes in dict keys

--- src/javascript/roman-time/roman-time.js
+++ src/javascript/roman-time/roman-time.js
@@ -43,13 +43,13 @@ function convertDecimalToRoman(Decimal) {
     let roman = "";
 
     let dict = {
-        "L": 50,
-        "XL": 40,
-        "X": 10,
-        "IX": 9,
-        "V": 5,
-        "IV": 4,
-        "I": 1
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
     };
 
     for (let key in dict) {
